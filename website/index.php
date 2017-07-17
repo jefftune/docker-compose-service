e7ad6dea3eeab4de382bce12c7b6a0bba2a71ed8
Jeff Tanner <jefft@tune.com>
2017-07-17 15:52:46 -0700

    [3] Cleanup

--- website/index.php
+++ website/index.php
@@ -7,7 +7,7 @@
         <h1>Welcome to my shop</h1>
         <ul>
             <?php
-                $json = file_get_contents('http://product-service');
+                $json = file_get_contents('http://products-service');
                 $obj = json_decode($json);
 
                 $products = $obj->products;
